f0bae93f7701e656308233db13bcd48fc845d2e5
Gabriel Lewertowski <glewerto@gmail.com>
2015-02-27 18:32:53 +0100

    subst_typ

--- F01_Defs.v
+++ F01_Defs.v
@@ -2,6 +2,7 @@ Require Export Utf8.
 Require Export Arith.
 Require Export Max.
 Require Export Omega.
+Require Export Relations.
 
 Ltac inv H := inversion H; try subst; clear H.
 
@@ -262,3 +263,13 @@ Proof.
   + inv H. inv H0. apply f_equal. eapply IHT; try eassumption.
     simpl. rewrite H2. reflexivity.
 Qed.
+
+(* substitution de X par U dans t *)
+Fixpoint subst_typ X U t :=
+         match t with
+             |Var _ => t
+             |Lam T t1 => Lam (tsubst X U T) (subst_typ (S X) (tshift 0 U) t1)
+             |App t1 t2 => App (subst_typ X U t1) (subst_typ X U t2)
+             |Abs k t1 => Abs k (subst_typ (S X) (tshift 0 U) t1)
+             |AppT t1 T => AppT (subst_typ X U t1) (tsubst X U T)
+         end.
